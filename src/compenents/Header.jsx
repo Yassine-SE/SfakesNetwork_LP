9bd051423de511f47b38f5f3cb4fde6f2b8f54e7
Yassine-SE <ytarkaoui@gmail.com>
2024-12-12 11:15:45 +0100

    responsive

--- src/compenents/Header.jsx
+++ src/compenents/Header.jsx
@@ -1,30 +1,36 @@
+import { useState } from "react";
 import "../style/Header.scss"
 import logo from "../assets/Ouispad-logo-white.svg"
 
 const Header = () => {
+     const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+     const toggleMenu = () => {
+          setIsMenuOpen(!isMenuOpen);
+     };
+
      return (
           <header className="header-container">
-
                <div className="logo">
-                    <img src={logo} alt="" />
+                    <img src={logo} alt="Ouispad Logo" />
                </div>
 
-               <div className="header-nav">
+               <button className="mobile-menu-button" onClick={toggleMenu}>
+                    {isMenuOpen ? "✕" : "☰"}
+               </button>
 
+               <div className={`header-nav ${isMenuOpen ? 'active' : ''}`}>
                     <nav className="nav">
-                         <a href="/">Accueil</a>
-                         <a href="/#Vision">Vision</a>
-                         <a href="/#Feature">Fonctionnalités</a>
-                         {/* <a href="#Contact">Contact</a> */}
+                         <a href="/" onClick={() => setIsMenuOpen(false)}>Accueil</a>
+                         <a href="/#Vision" onClick={() => setIsMenuOpen(false)}>Vision</a>
+                         <a href="/#Feature" onClick={() => setIsMenuOpen(false)}>Fonctionnalités</a>
                     </nav>
 
                     <nav className="nav">
-                         <a href="https://app.ouispad.fr/login">Login</a>
-                         <a className="primary-button small" href="https://app.ouispad.fr/signup">Essayer maintenant</a>
+                         <a href="https://app.ouispad.fr/login" onClick={() => setIsMenuOpen(false)}>Se connecter</a>
+                         <a className="primary-button small" href="https://app.ouispad.fr/signup" onClick={() => setIsMenuOpen(false)}>Essayer maintenant</a>
                     </nav>
-
                </div>
-
           </header>
      );
 };
