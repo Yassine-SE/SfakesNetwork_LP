4690718779e49da459990a8ff78e81863a3548b9
Yassine-SE <ytarkaoui@gmail.com>
2025-02-28 12:10:44 +0000

    step images

--- src/compenents/Steps.jsx
+++ src/compenents/Steps.jsx
@@ -2,25 +2,28 @@ import { useState } from "react";
 import "../style/Steps.scss"
 
 import DefaultImg from "../assets/default_img.png"
+import StepOne from "../assets/1.png"
+import StepTwo from "../assets/2.png"
+import Stepthree from "../assets/3.png"
 
 const steps = [
      {
           id: 1,
           title: "1 - Renseignez-vos informations",
           content: "Répondez au questionnaire de notre IA pour lui permettre de mieux comprendre votre entreprise : secteur d’activité, client cible, objectifs…",
-          image: DefaultImg,
+          image: StepOne,
      },
      {
           id: 2,
           title: "2 - Finalisez votre publicité",
           content: "Grâce à ces informations, Ouispad génère automatiquement une publicité adaptée à votre activité et à vos futurs clients. Vous pouvez modifier le texte, l’image et les paramètres (ex : budget, ciblage…) selon vos préférences.", 
-          image: DefaultImg,
+          image: StepTwo,
      },
      {
           id: 3,
           title: "3 - Lancez votre campagne et suivez vos prospects",
           content: "Une fois votre publicité validée, elle est diffusée ! Il ne vous reste plus qu’à attendre vos premiers prospects et à les contacter. Un espace dédié vous permet de suivre et gérer efficacement vos leads.",
-          image: DefaultImg,
+          image: Stepthree,
      },
 ];
 
